61d5ccd196908d1ca4885c562a6fc796e15eebe4
Renan de Carvalho Ribeiro <renancr176@gmail.com>
2020-04-01 16:30:28 -0300

    Criado página de compras (shop)

--- src/App.js
+++ src/App.js
@@ -3,37 +3,14 @@ import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/home-page.component';
-
-const HatsPage = () => (
-    <h1>Sample of Hats Page</h1>
-);
-
-const JacketsPage = () => (
-  <h1>Sample of Jackets Page</h1>
-);
-
-const SneakersPage = () => (
-  <h1>Sample of Sneakers Page</h1>
-);
-
-const WomensPage = () => (
-  <h1>Sample of Womens Page</h1>
-);
-
-const MensPage = () => (
-  <h1>Sample of Mens Page</h1>
-);
+import ShopPage from './pages/shop/shop-page.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/hats' component={HatsPage} />
-        <Route exact path='/jackets' component={JacketsPage} />
-        <Route exact path='/sneakers' component={SneakersPage} />
-        <Route exact path='/womens' component={WomensPage} />
-        <Route exact path='/mens' component={MensPage} />
+        <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
---
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './preview-collection.styles.scss';
+
+import CollectionItem from '../collection-item/collection-item.component';
+
+const PreviewCollection = ({title, items}) => (
+    <div className="collection-preview">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <div className="preview">
+            {
+                items
+                .filter((item, idx) => idx < 4)
+                .map(({id, ...item}) => (
+                    <CollectionItem key={id} {...item}/>
+                ))
+            }
+        </div>
+    </div>
+);
+
+export default PreviewCollection;

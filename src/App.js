904a5f55a9862aa6d41d0e86db6af2131364367c
Renan de Carvalho Ribeiro <renancr176@gmail.com>
2020-04-01 15:15:26 -0300

    Adicionado navegação ao projeto.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/home-page.component';
 
+const HatsPage = () => (
+    <h1>Sample of Hats Page</h1>
+);
+
+const JacketsPage = () => (
+  <h1>Sample of Jackets Page</h1>
+);
+
+const SneakersPage = () => (
+  <h1>Sample of Sneakers Page</h1>
+);
+
+const WomensPage = () => (
+  <h1>Sample of Womens Page</h1>
+);
+
+const MensPage = () => (
+  <h1>Sample of Mens Page</h1>
+);
+
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/jackets' component={JacketsPage} />
+        <Route exact path='/sneakers' component={SneakersPage} />
+        <Route exact path='/womens' component={WomensPage} />
+        <Route exact path='/mens' component={MensPage} />
+      </Switch>
+    </div>
   );
 }
 

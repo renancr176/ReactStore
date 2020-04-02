a8934b1bb30b85334c944aea6d1ea94bd9084928
Renan de Carvalho Ribeiro <renancr176@gmail.com>
2020-04-01 23:27:29 -0300

    Iniciado a criação da página de sing in e sing up

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
 import Header from './components/header/header.component';
-import HomePage from './pages/homepage/home-page.component';
-import ShopPage from './pages/shop/shop-page.component';
+import SigInUpPage from './pages/sign-in-up-page/sign-in-up-page.component';
+import HomePage from './pages/home-page/home-page.component';
+import ShopPage from './pages/shop-page/shop-page.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
+        <Route exact path='/signin' component={SigInUpPage}/>
         <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
---
+++ src/pages/sign-in-up-page/sign-in-up-page.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './sign-in-up-page.styles.scss';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SigInUpPage = () => (
+    <div className="sign-in-up-page">
+        <SignIn/>
+    </div>
+);
+
+export default SigInUpPage;

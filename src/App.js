da25ac5f5613b5f4c215fffb64fe7133bd765c0e
Renan de Carvalho Ribeiro <renancr176@gmail.com>
2020-04-01 22:02:27 -0300

    Criado o componente header com o menu de navegação.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/home-page.component';
 import ShopPage from './pages/shop/shop-page.component';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
